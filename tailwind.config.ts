a1cd225c737f43e565dd2d23aec9a7a257cda1d3
Johurul Haque <johurulhaquejony@gmail.com>
2024-11-29 07:28:46 +0600

    init project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,16 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        '2xl': '1400px',
       },
     },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
